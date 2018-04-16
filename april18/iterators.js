d1925f890820e53b047d063080323a9967868065
Daniel Santos <daniel@brasileiro.ca>
2018-04-16 10:52:48 -0400

    starting with iterators

--- april18/iterators.js
+++ april18/iterators.js
@@ -1,1 +1,23 @@
 // let's get started
+
+const dragons = [
+    'cool dragon',
+    'angry dragon',
+    'nasty dragon'
+];
+
+for ( const dragon of dragons ) {
+    dragon
+}
+
+const iterator = dragons[ Symbol.iterator ]();
+iterator.next() /*?*/
+iterator.next() //?
+iterator.next() //?
+iterator.next() //?
+iterator.next() //?
+
+for ( const char of dragons[ 0 ] ) {
+    // char;
+    console.log( char );
+}
