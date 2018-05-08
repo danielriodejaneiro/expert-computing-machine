26b8f50c26343bccaa8961e35be253124a030823
Daniel Santos <daniel@brasileiro.ca>
2018-05-07 22:11:39 -0400

    Added some tests (but still got 55% on Codility score).

--- may07/soti.js
+++ may07/soti.js
@@ -1,18 +1,22 @@
 function solution(a) {
-    console.log('***************************');
+    // console.log('***************************');
     a = a.sort((a, b) => a - b);
-    console.log('sorted => ', a);
+    // console.log('sorted => ', a);
     let result = -1;
     let done = false;
     a.forEach((current, i, a) => {
             if (a[i + 1] <= current + 1 && current > 0) {
-                console.log(i, 'current:', current, ' next:', a[i + 1], ' => NOT VIABLE');
+                // console.log(i, 'current:', current, ' next:', a[i + 1], ' => NOT VIABLE');
             } else if (a[i + 1] > current + 1 && current > 0) {
-                console.log(i, 'current', current, ' next: ', a[i + 1], ' => VIABLE <===');
+                // console.log(i, 'current', current, ' next: ', a[i + 1], ' => VIABLE <===');
                 !done ? result = current + 1 : null;
                 done = true;
             } else {
-                console.log(i, '- Value is not positive.');
+                if (i+1 === a.length && !done) {
+                    result = current + 1;
+                } else {
+                    // console.log(i, '- Value is not positive.');
+                }
             }
         }
     );
@@ -20,12 +24,14 @@ function solution(a) {
     return result < 1 ? 1 : result;
 }
 
+a0 = [1, 2, 3];
 a1 = [1, 3, 6, 4, 1, 2];
 a2 = [1];
 a3 = [1345, 3453, 126, 6754, 71, 2];
 a4 = [];
 a5 = [132311345, -3453, -126, 6754, 71, , 2, 0];
 
+console.log('RESULT:', solution(a0));
 console.log('RESULT:', solution(a1));
 console.log('RESULT:', solution(a2));
 console.log('RESULT:', solution(a3));
